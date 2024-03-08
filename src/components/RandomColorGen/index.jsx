f679267124bb23231be0b9b3d4f45b9e3af14a88
Darren Brain <darren.brain@gmail.com>
2024-03-08 18:55:07 -0500

    Add buttons and state

--- src/components/RandomColorGen/index.jsx
+++ src/components/RandomColorGen/index.jsx
@@ -3,8 +3,14 @@ import './styles.css';
 
 export default function RandomColorGen() {
 
+    const [currentColor, setCurrentColor] = useState([0, 0, 0])
+    const [displayFormat, setDisplayFormat] = useState('hex')
 
-    return <div>
-            RandomColorGen
+
+    return <div className="wrapper">
+            <button onClick={() => setDisplayFormat('hex')} className="randomColorBtn">Hex Color</button>
+            <button onClick={() => setDisplayFormat('rgb')} className="randomColorBtn">RGB Color</button>
+            <button onClick={() => generateColor()} className="randomColorBtn">Random Color</button>
+
         </div>
 }
