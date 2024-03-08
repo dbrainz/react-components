ce8039534904a0ccc39e7464d02e6abe29b68bf5
Darren Brain <darren.brain@gmail.com>
2024-03-08 14:44:18 -0500

    Add base Accordian framework

---
+++ src/components/Accordian/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id: '1',
+        question: 'Question 1',
+        answer: 'Amswer 1'
+    },
+    {
+        id: '2',
+        question: 'Question 2',
+        answer: 'Amswer 2'
+    },
+    {
+        id: '3',
+        question: 'Question 3',
+        answer: 'Amswer 3'
+    },
+    {
+        id: '4',
+        question: 'Question 4',
+        answer: 'Amswer 4'
+    }
+];
+
+export default data;
---
+++ src/components/Accordian/index.jsx
@@ -0,0 +1,9 @@
+import data from './data';
+
+export default function Accordian() {
+
+    return <div>
+        Accordian
+    </div>
+
+}
