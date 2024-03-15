1351d6bc0799d49a36f37a6d10d14466e2c9227f
Darren Brain <darren.brain@gmail.com>
2024-03-14 20:10:56 -0400

    Add API call to picsum to get test data

--- src/components/ImageSlider/index.jsx
+++ src/components/ImageSlider/index.jsx
@@ -1,11 +1,24 @@
 import { useState } from "react";
 import './styles.css';
 
-export default function ImageSlider() {
-    
 
+
+export default function ImageSlider({url, limit}) {
+    
+    const [currentPic, setCurrentPic] = useState(0);
+    const [picData, setPicData] = useState(null);
+
+    fetch("https://picsum.photos/v2/list?page=1&limit=10")
+        .then( (data) => setData(data))
+        .catch( error => 
+            console.log(error)
+            )
 
     return <div className='image-slider'>
-        Image Slider
+        { picData ?  
+        <span className="leftArrow">{"<"}</span>
+        <img src={} />
+        <span className="rightArrow">{">"}</span> :
+}
     </div>
 }
