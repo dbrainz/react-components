2dc71198c4039a77b8f37e643c4aa5d9d448a930
Darren Brain <darren.brain@gmail.com>
2024-03-19 18:19:01 -0400

    Add css to arrow buttons

--- src/components/ImageSlider/index.jsx
+++ src/components/ImageSlider/index.jsx
@@ -8,24 +8,36 @@ export default function ImageSlider({url, limit=5}) {
     const [currentPic, setCurrentPic] = useState(0);
     const [picData, setPicData] = useState(null);
 
-        useEffect(() => {
-            fetch(url)
-                .then((response) => response.json())
-                .then( (data) => {
-                    console.log(data)
-                    setPicData(data)
-                })
-                .catch( error => 
-                    console.log(error)
-                    )
-        }, [url])
+    function goPrevPic() {
+        if (currentPic>0) {
+            setCurrentPic(currentPic-1);
+        }
+    }
+
+    function goNextPic() {
+        if (currentPic<picData.length-1) {
+            setCurrentPic(currentPic+1)
+        }
+    }
+
+    useEffect(() => {
+        fetch(url)
+            .then((response) => response.json())
+            .then( (data) => {
+                console.log(data)
+                setPicData(data)
+            })
+            .catch( error => 
+                console.log(error)
+                )
+    }, [url])
 
     return <div className='image-slider'>
         { picData ?
             <div className="pic-window">
-                <span className="left-arrow">{"<"}</span>
+                <span onClick = { () => goPrevPic()} className="left-arrow">{"<"}</span>
                 <img className="slider-pic" src={picData[currentPic].download_url} />
-                <span className="right-arrow">{">"}</span>
+                <span onClick = { () => goNextPic()} className="right-arrow">{">"}</span>
             </div> :
             <div>
                 <p>Loading...</p>
